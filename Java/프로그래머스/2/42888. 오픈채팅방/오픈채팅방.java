27c484f729842f4bbd324779dec24e178c40407a
Jimin Byun <fraise3838@naver.com>
2025-02-17 10:57:05 +0900

    [level 2] Title: 오픈채팅방, Time: 137.54 ms, Memory: 176 MB -BaekjoonHub

---
+++ Java/프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class Solution {
+	
+	public String[] solution(String[] record) {
+       HashMap<String, String> msg = new HashMap<>();
+       msg.put("Enter", "님이 들어왔습니다.");
+       msg.put("Leave", "님이 나갔습니다.");
+       
+       HashMap<String, String> uid = new HashMap<>();
+       
+       for(String s : record) {
+    	   String[] cmd = s.split(" ");
+    	   if(cmd.length == 3) {
+    		   uid.put(cmd[1], cmd[2]);
+    	   }
+       }
+       
+       ArrayList<String> answer = new ArrayList<>();
+       for(String s : record) {
+    	   String[] cmd = s.split(" ");
+    	   if(msg.containsKey(cmd[0])) {
+    		   answer.add(uid.get(cmd[1]) + msg.get(cmd[0]));
+    	   }
+       }
+       return answer.toArray(new String[0]);
+    }
+
+}
